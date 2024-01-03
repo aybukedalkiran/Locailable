d8448ec252f9de48a5284e54e731c6440e5017e2
asyacora <asya.cora@gmail.com>
2024-01-03 21:34:30 +0300

    Merge changes from remote repository

--- business/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Business, Availability, Review
-
-admin.site.register(Business)
-admin.site.register(Availability)
-admin.site.register(Review)
